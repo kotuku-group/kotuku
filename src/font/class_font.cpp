af0b58d936a35d32b8d4b2a0ca52db31f438fe45
Paul Manias <31156549+paul-manias@users.noreply.github.com>
2026-04-22 10:31:57 +0100

    [Core] CreateLink() now supported in Windows; Updated some stale C functionality

--- src/font/class_font.cpp
+++ src/font/class_font.cpp
@@ -855,7 +855,7 @@ static ERR draw_bitmap_font(extFont *Self)
       }
       else if (*str IS '\t') {
          int16_t tabwidth = (Self->prvChar['o'].Advance * Self->GlyphSpacing) * Self->TabSize;
-         if (tabwidth) dxcoord = Self->X + pf::roundup(dxcoord - Self->X, tabwidth);
+         if (tabwidth) dxcoord = Self->X + tab_advance<int>(dxcoord - Self->X, tabwidth);
          str++;
       }
       else {
